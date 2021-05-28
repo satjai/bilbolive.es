37096e6c35ea6ec8def9570e2ee2e0d3fbf3f050
satjai <satjaisat@gmail.com>
2021-05-28 18:22:47 +0000

    asdf

--- src/Views/DisplayView.php
+++ src/Views/DisplayView.php
@@ -5,11 +5,17 @@ while($p = $stmt->fetch(PDO::FETCH_OBJ)){
  // print_r( $data[0]->nplato);
  //print_r($_SERVER['SERVER_NAME']);
   ?>  
-  <form id="platos" name="platos" enctype="multipart/form-data" action="http://<?php echo $_SERVER['SERVER_NAME'];?>/Index/page" method="post">
-  <input type="text" name="code" id="code" value="es">
+  <form id="platos" name="platos" enctype="multipart/form-data" action="#" method="post">
+    <select name="code">
+    	<option value="es">Select Language</option>
+    	<option value="es">Spanish</option>
+    	<option value="en">English</option>
+
+    </select>
      <input type="text" name="ide" id="ide" value="1">
   </form>
   <button type="button" id="enviar" style="cursor:pointer" > ENVIAR </button>
+
   <div id="target"> target
   </div>  
    <div id="target2">  
--- src/script.js
+++ src/script.js
@@ -1,55 +1,23 @@
 $(document).ready(function(){ 
-  /* $.ajax({
-			    type: "GET",
-			   url: "http://bilbolive.es/Index/page",
-			     data: 1,
-			   success: function(data){
-                   $("#target2").html(data);
-               }});
-			   */
 
-$("#enviar").click(function(){ 
+	$("#enviar").click(function(){ 
+		var unindexed_array =  $("#platos").serializeArray();
+		var indexed_array = {};
+		$.map(unindexed_array, function(n, i){
+			indexed_array[n['name']] = n['value'];
+		});
+		console.log(indexed_array);
 
-  
-       var code= jQuery("#code").val();
-	   var ide = jQuery("#ide").val();
-		 
-		   $.ajax({
-			   data:{
-			 code:code,
-             ide: ide,
-             },
-			    type: "POST",
-			    url: "http://bilbolive.es/index.php?controller=Index&task=page",
-			  success: function(data){
-                   $("#target").html(data);
-               }});
+
+		$.ajax({
+			data: indexed_array,
+			type: "POST",
+			url: "http://bilbolive.es/index.php?controller=Index&task=page",
+			success: function(data){
+				$("#target").html(data);
+			}});
 		
-
-		  /*  jQuery.ajax({
-        type: "POST",
-        url: "http://bilbolive.es/index.php?controller=Index&task=page",
-        data: data,
-        success: function(data){
-		 
-          jQuery('#target').html(jQuery(data).fadeIn('slow')); 
-      
-        }
-    }); */
-	 
-		
-    
-	 
-	
- 
- 
- 
-	 
-
-
-
-
-});  
+    });
 });
 
 
